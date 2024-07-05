b3d1633454b6539d07645435e353f641501d4ee4
soru <ityouna007@gmail.com>
2024-07-05 22:50:30 +0900

    1~5 finish

---
+++ 第１章(まずは慣れよう)/ex0101.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void){
+  printf("答えは%d\n", 15 + 37);
+  return 0;
+}
---
+++ 第１章(まずは慣れよう)/ex0104.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("もしもし。\nこんにちは。\n\nそれでは。\n");
+  return 0;
+}
